6acac6bc3a5a7992cff40b168eb9d971ae11a435
arryboom <888@988587.com>
2019-07-19 17:01:26 +0800

    up

--- bilibili.js
+++ bilibili.js
@@ -1,20 +1,22 @@
 // ==UserScript==
 // @name         BÕ¾ÎÞµ¯Ä»
 // @namespace    http://tampermonkey.net/
-// @version      0.16
+// @require https://cdn.staticfile.org/jquery/2.0.3/jquery.min.js
+// @version      0.21
 // @description  ×Ô¶¯ÆÁ±ÎËùÓÐbilibiliµ¯Ä»
 // @github	 https://www.github.com/arryboom/nodanmu
 // @author       arryboom
 // @match        *://*.bilibili.com/*
+// @run-at document-idle
 // ==/UserScript==
 
 (function() {
     'use strict';
-setTimeout(function(){
-	var nodanmu_a=$("div[class='bilibili-player-video-danmaku-switch bui bui-switch']").eq(0).children(".bui-checkbox");
+	var lasturl;
+	var no_danmu=function(){var nodanmu_a=$("div[class='bilibili-player-video-danmaku-switch bui bui-switch']").eq(0).children(".bui-checkbox");
 	var nodanmu_b=$("div[class='bilibili-player-video-btn bilibili-player-video-btn-danmaku']").eq(0).attr("data-text");
 	var nodanmu_c=$("div[class='bilibili-live-player-video-controller-hide-danmaku-container']");
-	console.log(nodanmu_b);
+	//console.log(nodanmu_b);
 	if((typeof(nodanmu_b)=="undefined") && (nodanmu_a.size()!=0)){
 	nodanmu_a.click();
 	}
@@ -25,6 +27,18 @@ setTimeout(function(){
 	{
 		$("button[data-title='Òþ²Øµ¯Ä»']").click();
 	}
+    $("div").remove(".danmu-module");
 	console.log("#####Bilibili_NoDanMu#####");
-    },3000);
+	lasturl=document.URL;
+    };
+setTimeout(function(){
+	no_danmu();
+},3000);
+setInterval(function(){
+	var curl=document.URL;
+    $("div").remove(".van-danmu");
+	if (curl!=lasturl){
+		no_danmu();
+	}
+}, 3000); 
 })();
--- douyu.js
+++ douyu.js
@@ -1,22 +1,57 @@
 // ==UserScript==
 // @name         斗鱼无弹幕
 // @namespace    http://tampermonkey.net/
-// @version      0.12
+// @version      0.21
 // @description  自动屏蔽所有斗鱼直播间弹幕
 // @author       arryboom
 // @match        *://www.douyu.com/*
 // @grant        none
+// @run-at document-idle
 // ==/UserScript==
+
 
 (function() {
     'use strict';
     var time = 0;
+	var tstatus=0;
+	var tastatus=0;
     /*document.querySelector==$*/
-setTimeout(function(){document.querySelector("div[class^='showdanmu-']").click();console.log("###Douyu###");},5000);
+setTimeout(function(){document.querySelector("div[class^='showdanmu-']").click();console.log("###Douyu###");},7000);
 setTimeout(function(){
-tstatus=document.querySelector("span[class^='shie-input']").attr("data-shield-status");
-if (tstatus=="0"){
-$("span[class^='shie-input']").children(".shie-checkbox-icon").click();
-};},5000);
-/*$('.PlayerCaseSub-Main').hide();隐藏聊天区*/
+tstatus=document.querySelector("div[class='ShieldTool-enter']").firstChild.className.indexOf("is-checked");
+tastatus=document.querySelector("div[class='ShieldTool-enter']").firstChild.className.indexOf("is-allChecked");
+if (tstatus<0 && tastatus<0){
+/*$("span[class^='shie-input']").children(".shie-checkbox-icon").click();*/
+console.log("enter t");
+document.querySelector("h5[class^='ShieldTool-check']").click();
+//$('.PlayerCaseSub-Main').hide();
+//$(".layout-Player-aside").hidden=1;
+
+//$(".layout-Bottom").hidden=1;
+};
+if (tastatus>0 && tstatus<0){
+/*$("span[class^='shie-input']").children(".shie-checkbox-icon").click();*/
+console.log("enter ta");
+document.querySelector("h5[class^='ShieldTool-check']").click();
+setTimeout(function(){document.querySelector("h5[class^='ShieldTool-check']").click();},2000);
+//$('.PlayerCaseSub-Main').hide();
+//$(".layout-Player-aside").hidden=1;
+
+//$(".layout-Bottom").hidden=1;
+};
+
+},5000);
+/*隐藏聊天区*/
+//---------------------------------------
+/*
+setTimeout(function(){
+//document.querySelector("div[class='layout-Player-aside']").style.visibility="hidden";
+document.querySelector("div[class='layout-Player-aside']").style.display="none";
+document.querySelector("div[class='layout-Bottom']").style.display="none";
+document.querySelector("div[class='LotteryContainer']").style.display="none";
+//LotteryContainer
+},5000);
+*/
+//----------------------------------
+/*隐藏聊天区*/
 })();
